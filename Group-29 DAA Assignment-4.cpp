5971dfb7b3c50a3468c4f289f1b75c1a75cbe49c
iit2019089 <80326274+iit2019089@users.noreply.github.com>
2021-04-17 16:20:04 +0530

    bug fixes in c++ code

--- Group-29 DAA Assignment-4.cpp
+++ Group-29 DAA Assignment-4.cpp
@@ -1,39 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int floorBS(int arr[], int low,	int high, int x)
+int floorBS(int arr[], int low, int high, int x)
 {
-	if (low > high)
-		return -1;
-	if (x >= arr[high])
-		return high;
-	int mid = (low + high) / 2;
-	if (arr[mid] == x)
-		return mid;
-	if (mid > 0 && arr[mid - 1] <= x && x < arr[mid])
-		return mid - 1;
-	if (x < arr[mid])
-		return floorBS(	arr, low, mid - 1, x);
-	return floorBS(arr, mid + 1, high, x);
+    if (low > high)
+        return -1;
+    if (x >= arr[high])
+        return high;
+    int mid = (low + high) / 2;
+    if (arr[mid] == x)
+        return mid;
+    if (mid > 0 && arr[mid - 1] <= x && x < arr[mid])
+        return mid - 1;
+    if (x < arr[mid])
+        return floorBS(arr, low, mid - 1, x);
+    return floorBS(arr, mid + 1, high, x);
 }
 
 int main()
 {
-	int arr[7];
-	arr[i]=rand()%10;
-	for(int i=1;i<7;i++){
-		arr[i]=arr[i-1]+((rand()%10)+1)
-	}
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = arr[0]+(rand()%(arr[6]-arr[0]));
-	int index = floorBS(arr, 0, n - 1, x);
-	for(int i=0;i<7;i++){
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-	if (index == -1)
-		cout<<"Floor of "<<x<<" doesn't exist in array";
-	else
-		cout<<"Floor of "<<x<<" is "<<arr[index];
-	return 0;
+    int arr[7];
+    arr[0] = rand() % 10;
+    for (int i = 1; i < 7; i++)
+    {
+        arr[i] = arr[i - 1] + ((rand() % 10) + 1);
+    }
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = arr[0] + (rand() % (arr[6] - arr[0]));
+    int index = floorBS(arr, 0, n - 1, x);
+    for (int i = 0; i < 7; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    if (index == -1)
+        cout << "Floor of " << x << " doesn't exist in array";
+    else
+        cout << "Floor of " << x << " is " << arr[index];
+    return 0;
 }
